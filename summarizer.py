db5b6b1c71ce4da547d44ea572a348cecd4bb19b
Fed <federicoulfo@gmail.com>
2023-06-26 14:07:34 -0400

    up

--- summarizer.py
+++ summarizer.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import openai
 from datetime import datetime
 
@@ -13,29 +14,49 @@ def create_event_file(date, topic, participants, transcription, summary):
         file.write(summary)
 
 def update_readme(date, topic):
-    with open('README.md', 'a') as readme:
-        readme.write(f"\n- **{date}** » [{topic}](events/wpw-{date}.md)")
+    with open('README.md', 'r+') as readme:
+        content = readme.read()
+        position = content.find("## WPW Events")
+        position = content.find('\n', position) + 1
+        line_to_insert = f"- **{date}** » [{topic}](events/wpw-{date}.md)\n"
+        content = content[:position] + line_to_insert + content[position:]
+        readme.seek(0)
+        readme.write(content)
 
 def get_summary(transcription):
-    openai.api_key = 'your-api-key'
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Summarize the following meeting transcription: {transcription}"},
-        ]
-    )
-    return response['choices'][0]['message']['content']
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    chunks = [transcription[i:i + 4090] for i in range(0, len(transcription), 4090)]
+    full_summary = ""
+    for chunk in chunks:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a video meeting assistant."},
+                {"role": "user", "content": f"Summarize the video meeting text into a list of topics and takes discussed, with links if any: {chunk}"},
+            ]
+        )
+        full_summary += response['choices'][0]['message']['content']
+    return full_summary
 
 def format_date(date):
     date_object = datetime.strptime(date, "%Y-%m-%d")
     return date_object.strftime("%B %d, %Y")
 
-def create_event(date, topic, participants, transcription):
+def create_event(transcription_path):
+    date = input("Enter the date of the meeting (YYYY-MM-DD): ")
+    topic = input("Enter the topic of the meeting: ")
+    participants = input("Enter the attendees of the meeting (comma-separated): ")
+
+    with open(transcription_path, 'r') as file:
+        transcription = file.read()
+
     formatted_date = format_date(date)
     summary = get_summary(transcription)
     create_event_file(formatted_date, topic, participants, transcription, summary)
     update_readme(formatted_date, topic)
 
-# Use the function to create an event
-create_event("2023-06-30", "Crypto Discussion", "Alice, Bob, Charlie", "This is a test event.")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Summarize a meeting transcription.')
+    parser.add_argument('transcription_path', type=str, help='The path to the meeting transcription file.')
+    args = parser.parse_args()
+    create_event(args.transcription_path)
